4d508776b2539647041877ab81c91dde02b75b75
abrahdev <abraham@abrah.dev>
2026-02-11 21:02:46 +0100

    Change onBrokenLinks behavior from "throw" to "warn" in Docusaurus config

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -16,7 +16,7 @@ const config: Config = {
     v4: true, // Improve compatibility with the upcoming Docusaurus v4
   },
 
-  onBrokenLinks: "throw",
+  onBrokenLinks: "warn",
   onBrokenMarkdownLinks: "warn",
 
   i18n: {
