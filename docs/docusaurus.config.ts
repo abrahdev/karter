a9c81b56f05b8557545f7bf0c93cfbb7944a4b57
abrahdev <abraham@abrah.dev>
2026-02-12 15:44:26 +0100

    Update site title to "Karter Docs" in Docusaurus configuration

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -3,7 +3,7 @@ import type { Config } from "@docusaurus/types";
 import type * as Preset from "@docusaurus/preset-classic";
 
 const config: Config = {
-  title: "Karter",
+  title: "Karter Docs",
   tagline: "Open Source Vehicle Maintenance & Social Impact",
   favicon: "img/karter-favicon.svg",
 
